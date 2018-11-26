bf04dc7483dcd744794f67ee10a9ef95aa49250d
John Lapeyre <jlapeyre@users.noreply.github.com>
2018-11-26 14:07:38 +0100

    Remove the type Identity

--- src/IdentityMatrix.jl
+++ src/IdentityMatrix.jl
@@ -1,48 +1,17 @@
 """
     module IdentityMatrix
 
-This module overrides more generic methods for identity matrices.
-The methods are immediately available after loading.
-The methods apply to both `FillArrays.Eye`
-and to `IdentityMatrix.Identity`.
+Methods for `FillArrays.Eye`.
 """
 module IdentityMatrix
 
-import LinearAlgebra
-using FillArrays
+using LinearAlgebra, FillArrays
+import LinearAlgebra: triu, triu!, tril, tril!
+import Base: inv, permutedims
 
-export Identity, eye, isorthogonal, isunitary, Idents
+export identitymatrix, materialize
 
-"""
-    struct Identity{T<:Any} <: AbstractMatrix{T}
-
-Provides an alternative to `FillArrays.Eye` for a lazy identity matrix.
-"""
-struct Identity{T<:Any} <: AbstractMatrix{T}
-    ncols::Int
-end
-
-"""
-    Idents{T} = Union{Identity{T}, Eye{T}}
-
-Identity matrix types.
-Methods in `IdentityMatrix.jl` may break some usage of `Eye` elsewhere.
-By remvoving `Eye` from `Idents{T}`,
-you can isolate these methods from the rest of the system.
-The performance of the methods defined for `Idents` is identical for `Eye` and `Identity`.
-
-Methods not defined for `Idents` fall back to `AbstractMatrix` for `Identity` and to
-`Diagonal` for `Eye`.
-"""
-Idents{T} = Union{Identity{T}, Eye{T}}
-
-function Base.size(IM::Identity, d::Integer)
-    d < 1 && throw(ArgumentError("dimension must be ≥ 1, got $d"))
-    return d <= 2 ? IM.ncols : 1
-end
-Base.size(IM::Identity) = (IM.ncols, IM.ncols)
-
-checkuniquedim(IM::Idents) = size(IM, 1)
+# FIXME: replace checkuniquedim with something more efficient (from LinearAlgebra or Base)
 checkuniquedim(D::LinearAlgebra.Diagonal) = size(D, 1)
 checkuniquedim(AV::AbstractVector) = length(AV)
 
@@ -59,126 +28,162 @@ function checkuniquedim(A, B)
     return m1
 end
 
-## constructors for Identity
+# The fallback method for AbstractArray is already efficient for Eye for:
+# length, firstindex, lastindex, axes, checkbounds
 
-Identity(n::Integer) = Identity{Float64}(n)
-eye(::Type{T}, n::Integer) where T = Identity{T}(n)
-eye(n::Integer) = eye(Float64, n)
-function eye(::Type{T}, AM::AbstractMatrix) where T
-    dim = checkuniquedim(AM)
-    return Identity{T}(dim)
-end
-eye(AM::AbstractMatrix) = eye(eltype(AM), AM)
+# The fallback method (for Diagonal) is already efficient for Eye for:
+# getindex, factorize, real, float, ishermitian, issymmetric, isdiag, istriu,
+# istril, log, most trig functions
 
-## conversion
-
-Identity(IM::Identity) = IM
-Base.AbstractMatrix{T}(IM::Identity) where {T} = Identity{T}(size(IM, 1))
-
-# This is recommended in the manual. But the function `materialize` below
-# is significantly faster, especially for small matrices.
-#Base.Matrix(IM::Identity) = Matrix{eltype(IM)}(LinearAlgebra.I, size(IM)...)
-
-Base.Matrix(IM::Identity) = materialize(IM)
-Base.Array(IM::Identity) = Matrix(IM)
-
-Identity(D::LinearAlgebra.Diagonal) = all(isone, D.diag) ? Identity{eltype(D)}(size(D,1)) :
-    throw(InexactError(:Identity, Identity{eltype(D)}, D))
-
-LinearAlgebra.Diagonal(IM::Identity{T}) where T = LinearAlgebra.Diagonal(diag(IM))
-
-Base.eltype(::Type{Identity{T}}) where T = T
-Base.eltypeof(::Identity{T}) where T = T
-
-# Already efficient for Eye
-@inline function Base.getindex(IM::Identity{T}, i::Integer, j::Integer) where T
-    @boundscheck Base.checkbounds(IM, i, j)
-    return i == j ? one(eltype(IM)) : zero(eltype(IM))
+function Base.iterate(iter::Eye{T}, istate = (1, 1)) where T
+    (i::Int, j::Int) = istate
+    m = size(iter, 1)
+    return i > m ? nothing :
+        ((@inbounds getindex(iter, i, j)),
+         j == m ? (i + 1, 1) : (i, j + 1))
 end
 
-function Base.replace_in_print_matrix(A::Identity, i::Integer, j::Integer, s::AbstractString)
-    i == j ? s : Base.replace_with_centered_mark(s)
+# The default method for Diagonal returns Diagonal with a dense diagonal
+Base.imag(IM::Eye{T}) where T = Diagonal(Fill(zero(T), size(IM, 1)))
+Base.iszero(::Eye) = false
+Base.isone(::Eye) = true
+LinearAlgebra.isposdef(::Eye) = true
+LinearAlgebra.diag(IM::Eye{T}) where T = ones(T, size(IM, 1))
+Base.sum(IM::Eye) = convert(eltype(IM), size(IM, 1))
+Base.prod(IM::Eye) = zero(eltype(IM))
+Base.first(::Eye{T}) where T = one(T)
+Base.last(::Eye{T}) where T = one(T)
+Base.minimum(::Eye{T}) where T = zero(T)
+Base.maximum(::Eye{T}) where T = one(T)
+Base.extrema(IM::Eye) = (minimum(IM), maximum(IM)) # FIXME: implement extrema(IM, dims = dims)
+
+for f in (:permutedims, :triu, :triu!, :tril, :tril!, :inv) # These override inefficient methods for Eye
+    @eval ($f)(IM::Eye) = IM
 end
 
-include("Eye.jl")
-
-LinearAlgebra.factorize(IM::Identity) = IM
-
-Base.real(IM::Identity{T}) where T = IM
-Base.float(IM::Identity{T}) where T = Identity{float(T)}(size(IM, 1))
-
-LinearAlgebra.ishermitian(::Identity) = true
-LinearAlgebra.issymmetric(::Identity) = true
-
-# These may not be useful, since they don't exist in LinearAlgebra.
-isorthogonal(::Idents) = true
-isunitary(::Idents) = true
-
-LinearAlgebra.isdiag(::Identity) = true
-LinearAlgebra.istriu(::Identity) = true
-LinearAlgebra.istril(::Identity) = true
-
-import Base: transpose, adjoint, conj
-import Base: inv, permutedims
-import LinearAlgebra: triu, triu!, tril, tril!
-for f in (:transpose, :adjoint, :conj) # These are already effcient for Eye
-    @eval ($f)(IM::Identity) = IM
+# This agrees with diag for dense matrices in that any integer `k` is allowed.
+# It disagrees with diag for Diagonal, which throws an error when `abs(k)` is too large.
+function LinearAlgebra.diag(IM::Eye{T}, k::Integer) where T
+    k == 0 && return LinearAlgebra.diag(IM)
+    m = size(IM, 1)
+    (k > m || k < -m) && return T[]
+    return zeros(T, m - abs(k))
 end
 
-LinearAlgebra.tr(IM::Identity) = sum(IM)
-Base.log(IM::Identity) = LinearAlgebra.Diagonal(Fill(zero(eltype(IM)), size(IM,1)))
+LinearAlgebra.det(::Eye{T}) where T = one(T)
+LinearAlgebra.logdet(::Eye{T}) where T = log(one(T))
+Base.sqrt(IM::Eye) = IM
 
-# Matrix functions
-import Base: exp, cos, sin, tan, csc, sec, cot, cosh, sinh, tanh, csch, sech,
-    coth, acos, asin, atan, acsc, asec, acot, acosh, asinh, atanh, acsch, asech, acoth
-for f in (:exp, :cos, :sin, :tan, :csc, :sec, :cot,
-          :cosh, :sinh, :tanh, :sech, :coth,
-          :acos, :asin, :atan, :acsc, :asec, :acot,
-          :acosh, :asinh, :atanh, :acsch, :asech, :acoth)
-    @eval $f(IM::Identity) = LinearAlgebra.Diagonal(Fill($f(one(eltype(IM))), size(IM, 1)))
+# There are a few trig functions `f` for which `f(IM)` cannot be computed
+# at compile time. This is one way to solve this problem.
+const _cschval = csch(1.0)
+_mycsch(::Union{Type{Float64}, Type{Int}}) = _cschval
+_mycsch(::Type{T}) where T = csch(one(T))
+Base.csch(IM::Eye) = LinearAlgebra.Diagonal(Fill(_mycsch(eltype(IM)), size(IM, 1)))
+
+Base.:(^)(IM::Eye, p::Integer) = IM
+
+(Base.:/)(AM::AbstractMatrix, IM::Eye) = IM * AM
+(Base.:/)(AM::Eye, IM::Eye) = IM * AM
+(Base.:/)(AM::DenseMatrix, IM::Eye) = IM * AM
+
+function (Base.:*)(IM::Eye{T}, AV::AbstractVector{V}) where {T, V}
+    junk = checkuniquedim(IM, AV)
+    return convert(Vector{Base.promote_op(*, T, V)}, AV)
 end
 
-Base.:(==)(IMa::Identity, IMb::Identity) = size(IMa, 1) == size(IMb, 1)
-(Base.:-)(IM::Identity{T}) where T = -FillArrays.Eye{T}(size(IM, 1))
+function (Base.:*)(IM::Eye{T}, AM::AbstractMatrix{V}) where {T,V}
+    junk = checkuniquedim(IM, AM)
+    return convert(Matrix{Base.promote_op(*, T, V)}, AM)
+end
+(Base.:*)(AM::AbstractMatrix{T}, IM::Eye{V}) where {T, V} = IM * AM
+(Base.:*)(AM::Diagonal, IM::Eye) = IM * AM
+(Base.:*)(IMa::Eye{T}, IMb::Eye{V}) where {T, V} = Eye{Base.promote_op(*, T, V)}(size(IMa, 1))
 
-# Diagonal is already efficient
-# But, we must disambiguate
-function (Base.:+)(IMa::Idents{T}, IMb::Identity{V}) where {T,V}
-    d = checkuniquedim(IMa, IMb)
-    return LinearAlgebra.Diagonal(Fill(one(T) + one(V), d))
+function Base.kron(a::Eye{T}, b::AbstractMatrix{S}) where {T, S}
+    @assert ! Base.has_offset_axes(b)
+    R = zeros(Base.promote_op(*, T, S), size(a, 1) * size(b, 1), size(a, 2) * size(b, 2))
+    m = 1
+    for j = 1:size(a, 2)
+        for l = 1:size(b, 2)
+            for k = 1:size(b, 1)
+                R[m] = b[k,l]
+                m += 1
+            end
+            m += (size(a, 2) - 1) * size(b, 2)
+        end
+        m += size(b, 2)
+    end
+    return R
 end
 
-# This is a bit slow. But, we will discard it anyway.
-# Diagonal is already efficient
-function (Base.:-)(IMa::Identity{T}, IMb::Identity{V}) where {T,V}
-    d = checkuniquedim(IMa, IMb)
-    return Diagonal(Zeros{Base.promote_op(-, T, V)}(size(IMa)))
+function Base.kron(a::AbstractMatrix{T}, b::Eye{S}) where {T, S}
+    @assert ! Base.has_offset_axes(a)
+    R = zeros(Base.promote_op(*, T, S), size(a, 1) * size(b, 1), size(a, 2) * size(b, 2))
+    m = 1
+    for j = 1:size(a, 1)
+        for l = 1:size(b, 1)
+            for k = 1:size(a, 2)
+                R[m] = a[k,j]
+                m += size(b, 2)
+            end
+            m += 1
+        end
+        m -= size(b, 2)
+    end
+    return R
 end
 
-# Diagonal is already efficient
-(Base.:*)(x::Number, IM::Identity{V}) where V = FillArrays.Fill{Base.promote_op(*, typeof(x), V)}(x, size(IM, 1)) |> LinearAlgebra.Diagonal
+Base.kron(a::Eye{T}, b::Eye{T}) where T = Eye{T}(size(a, 1) * size(b, 1))
+Base.kron(a::Eye{T}, b::Eye{V}) where {T, V} = Eye{Base.promote_op(*, T, V)}((size(a, 1) * size(b, 1)))
 
-# Diagonal is already efficient
-(Base.:*)(IM::Identity, x::Number) = x * IM
+LinearAlgebra.eigvals(IM::Eye{T}) where T = diag(IM)
+LinearAlgebra.eigvecs(IM::Eye) = IM # method for Diagonal returns a material matrix
+LinearAlgebra.eigen(IM::Eye) = LinearAlgebra.Eigen(LinearAlgebra.eigvals(IM), LinearAlgebra.eigvecs(IM))
 
-# Diagonal is already efficient
-function (Base.:/)(IM::Identity{T}, x::Number) where T
-    return LinearAlgebra.Diagonal(Fill(one(T) / x, size(IM, 1)))
+####
+
+## materializng is a mystery
+
+# No. Today it is not faster. It is much slower
+# This is faster than Matrix(I, n, n)
+function identitymatrix(T::DataType, n::Int)
+    a = zeros(T, n, n)
+    @inbounds for i in 1:n # @inbounds does not change benchmark times
+        a[i, i] = 1  # A bit faster than linear indexing
+    end
+    return a
+end
+identitymatrix(n::Integer) = identitymatrix(Float64, n)
+
+# # The following method is ~7 times slower than the one below it.
+# # This is baffling. There should be no difference
+# # materialize(IM::Eye) = identitymatrix(eltype(IM), size(IM, 1))
+
+# Some days, this is a bit faster than Matrix{T}(I, n, n)
+function materialize(IM::Eye)
+    a = zeros(eltype(IM), size(IM))
+    @inbounds for i in 1:size(IM,1)  # @inbounds does not change benchmark times
+        a[i, i] = 1  # A bit faster than linear indexing
+    end
+    return a
 end
 
-# Diagonal is already efficient
-function (Base.:*)(IMa::Identity{T}, IMb::Identity{V}) where {T, V}
-    d = checkuniquedim(IMa, IMb)
-    return Identity{Base.promote_op(*, T, V)}(d)
-end
+# For Eye{T}, the fallback method only materializes the diagonal.
+# Here we create a dense matrix in agreement with `copymutable` for other `AbstractArray`
+#Base.copymutable(IM::Eye) = Matrix{eltype(IM)}(I, size(IM))
+Base.copymutable(IM::Eye) = materialize(IM)
+#Base.copymutable(IM::Eye) = identitymatrix(eltype(IM), size(IM, 1))
 
-# Diagonal is already efficient Not needed
-#(Base.:/)(IMa::Identity{T}, IMb::Identity{V}) where {T, V} = IMa * IMb
+ensuretype(::Type{T}, AM::AbstractArray{T}) where {T} =  AM
+# FIXME: .* is slower than convert
+ensuretype(::Type{T}, AM::AbstractArray{V, N}) where {T, V, N} = convert(Array{T, N}, AM)
 
-# Object on left should be factorization object
-# LinearAlgebra.ldiv!(IM::Identity{T}, AV::AbstractVector{T}) where {T} = one(T) .* AV
-# LinearAlgebra.ldiv!(IM::Identity{T}, AM::AbstractMatrix{T}) where {T} = one(T) .* AM
-
-
+# Put these last. The backslash confuses emacs.
+# Diagonal is already efficient. But, we use `Eye` to remove fatal method ambiguity intrduced
+# by the methods below.
+(Base.:\)(IMa::Eye{T}, IMb::Eye{V}) where {T, V} = IMa * IMb
+(Base.:\)(AM::AbstractMatrix{T}, IM::Eye{V}) where {T, V} = ensuretype(Base.promote_op(*, T, V), Base.inv(AM))
+(Base.:\)(IM::Eye{V}, AM::AbstractMatrix{T}) where {T, V} = ensuretype(Base.promote_op(*, T, V), AM)
 
 end # module
